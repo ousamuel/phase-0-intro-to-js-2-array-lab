0d527abb53c33e59ea1a7a00db143aa67ac60517
Samuel <samuelou510@gmail.com>
2023-06-13 15:55:11 -0400

    repeat arrays lab from phase 0 to phase 1

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+  cats.splice(cats.length - 1, 1);
+}
+
+function destructivelyRemoveFirstCat(){
+  cats.splice(cats[0], 1);
+}
+
+function appendCat(name){
+  return [...cats, name];
+}
+
+function prependCat(name){
+  return [name, ...cats];
+}
+
+function removeLastCat(){
+  return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat(){
+  return cats.slice(1);
+}
